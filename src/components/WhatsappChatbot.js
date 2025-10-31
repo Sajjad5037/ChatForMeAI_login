ae6c69fa4e98ee89e065270cee413f312c04380c
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-10-31 15:03:07 +0500

    Update WhatsappChatbot.js

--- src/components/WhatsappChatbot.js
+++ src/components/WhatsappChatbot.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import KnowledgeBaseUpload from "./KnowledgeBaseUpload";
 import PublicChatbot from "./PublicChatbot"; // <-- import public chatbot
-import "./BusinessChatbot.css";
+import "./WhatsappChatbot.css";
 import ApiUsage from "./ApiUsage";
 
 export default function WhatsappChatbot({ doctorData }) {
